055d933b4d87dd90312d613d244aa35b46181a6e
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-24 13:27:21 -0500

    Time: 36 ms (91.35%), Space: 13.7 MB (70.27%) - LeetHub

--- delete-and-earn/delete-and-earn.py
+++ delete-and-earn/delete-and-earn.py
@@ -1,18 +1,19 @@
-class Solution:
-    def deleteAndEarn(self, nums: List[int]) -> int:
+class Solution(object):
+    def deleteAndEarn(self, nums):
         if not nums:
             return 0
         
         maxi = max(nums)
         
-        arr = [0 for i in range(maxi +1)]
-
+        arr = [0 for i in range(maxi + 1)]
+        
         for i in nums:
             arr[i] += i
-            
+        
         nt_choose, choose = 0, 0
         for i in arr:
             nt_choose, choose = max(nt_choose, choose), i + nt_choose
         return max(nt_choose, choose)
+            
             
         
