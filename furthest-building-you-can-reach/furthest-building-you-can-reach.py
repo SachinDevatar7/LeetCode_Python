1483bb7f0c553ac96f16ac1010079f16457f26f9
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-26 11:49:46 -0500

    Time: 552 ms (98.41%), Space: 28.7 MB (70.18%) - LeetHub

--- furthest-building-you-can-reach/furthest-building-you-can-reach.py
+++ furthest-building-you-can-reach/furthest-building-you-can-reach.py
@@ -1,6 +1,12 @@
+# Check Notes
+
+# Why MinHeap?
+# I need a minHeap since I have to get to use min bricks from the pile of given bricks which I get through minHeap so it's pop can be done in O(logn) Time which makes perfect to use minHeap
+
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
-        ladderAllocate = [] # minHeap
+        # diff are the climb steps to make it which is inside my ladderAllocate
+        ladderAllocate = [] # minHeap to keep count of how many ladder used
         
         for i in range(len(heights) - 1):
             diff = heights[i+1] - heights[i]
@@ -8,9 +14,13 @@ class Solution:
             if diff <= 0:
                 continue    
             heapq.heappush(ladderAllocate, diff)
+            #print(ladderAllocate)
             
-            if len(ladderAllocate) > ladders:   
+            # Tell ladder count how many being used 
+            if len(ladderAllocate) > ladders: # tells used all ladder and now need to use bricks 
                 bricks -= heapq.heappop(ladderAllocate)
+                
+            #print(bricks)
                 
             if bricks < 0:
                 return i
