e7a05cf812fd8ad548cdb16c2c40fbdbc218d5e5
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-04-27 08:35:47 -0500

    Time: 16 ms (86.51%), Space: 13.5 MB (86.06%) - LeetHub

--- valid-parentheses/valid-parentheses.py
+++ valid-parentheses/valid-parentheses.py
@@ -1,27 +1,28 @@
-# Testcase: "){" , "()[]{}" 
-class Solution:
-    def isValid(self, s: str) -> bool:
-        #base case with odd form
-        if s == 0 or len(s) % 2 != 0:
+# Stack [LIFO] so why we are using is in order to process it as a pair we are using stack! with usage of stack you can keep track of each VALID Pair!
+
+class Solution(object):
+    def isValid(self, s):
+        if not s or len(s) % 2 != 0:
             return False
         
         stack = []
-        dic = { ')':'(', '}':'{', ']':'['}
         
-        for char in s:
-            if char in dic:
-                if len(stack) == 0  or dic[char] != stack.pop():
-                    return False
-            else:
-                stack.append(char)
-                
+        for i in s:
+            if i == '(':
+                stack.append(')')
+            elif i == '[':
+                stack.append(']')
+            elif i == '{':
+                stack.append('}')
+            elif not stack or i != stack.pop():
+                return False
+            
         if len(stack) == 0:
             return True
-        return False
-         
+        else:
+            return False
+            
+            
         
-
-                
-                
         
-         
+        
