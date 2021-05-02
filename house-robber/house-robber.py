a1130d14dfa9c3e352726a37f8421badfa8942de
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-05-02 11:28:24 -0500

    Time: 32 ms (58.26%), Space: 14.2 MB (51.76%) - LeetHub

--- house-robber/house-robber.py
+++ house-robber/house-robber.py
@@ -1,7 +1,12 @@
-class Solution(object):
-    def rob(self, nums):
-        nt_choose, choose= 0, 0 
-        for num in nums:
-            nt_choose, choose = max(nt_choose, choose), num + nt_choose
-        return max(nt_choose, choose)
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
         
+        arr = [0 for i in range(len(nums) + 1)]
+        
+        nt_choose, choose = 0 , 0
+        for i in nums:
+            nt_choose, choose = max(nt_choose, choose) , i + nt_choose
+        return max(nt_choose, choose)
+            
