76f62650ad5be10787a098c9abbee705cbd3d934
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-05-17 10:30:08 -0500

    Time: 900 ms (78.65%), Space: 13.7 MB (29.41%) - LeetHub

--- longest-palindromic-substring/longest-palindromic-substring.py
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -1,40 +1,78 @@
-class Solution:
-    def longestPalindrome(self, s: str) -> str:
+class Solution(object):
+    def longestPalindrome(self, s):
         if not s or len(s) == 0:
             return s
         
         res = ''
         for i in range(len(s)):
             odd = self.helper(s, i, i)
-            even = self.helper(s, i, i+1)
+            even = self.helper(s, i, i+ 1)
             
             res = max(odd, even, res, key = len)
+            
         return res
     
-    def helper(self, s, l, r):
-        while l >= 0 and r < len(s) and s[l] == s[r]:
-            l -= 1
-            r += 1
-            # print(l,r)
-        return (s[l + 1 : r])
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+    def helper(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+            
+        return s[left+1 : right]
     
-#https://www.pythoncentral.io/how-to-slice-listsarrays-and-tuples-in-python/
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ #length = len(s)
+        
+#         def expand_center(i,j):
+#             while i>=0 and j < len(s) and s[i] == s[j]:
+                
+#                 i -= 1
+#                 j += 1 
+                
+#             return s[i + 1 : j]
+        
+#         result = ''
+        
+#         for i in range(length):
+#             temp_palin = expand_center(i,i) # odd
+
+#             if len(temp_palin) > len(result):
+#                 result = temp_palin
+        
+#             temp_palin = expand_center(i, i+ 1) # even 
+
+#             if len(temp_palin) > len(result):
+#                 result = temp_palin
+            
+#         return result
+            
+        
+        
+        
