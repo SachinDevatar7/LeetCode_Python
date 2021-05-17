90aa788fdaf89d7dc2ca3b9f2a183d376a994bb9
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-05-17 09:42:01 -0500

    Time: 940 ms (80.72%), Space: 14.3 MB (61.51%) - LeetHub

---
+++ longest-palindromic-substring/longest-palindromic-substring.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if not s or len(s) == 0:
+            return s
+        
+        res = ''
+        for i in range(len(s)):
+            odd = self.helper(s, i, i)
+            even = self.helper(s, i, i+1)
+            
+            res = max(odd, even, res, key = len)
+        return res
+    
+    def helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+            # print(l,r)
+        return (s[l + 1 : r])
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    
+#https://www.pythoncentral.io/how-to-slice-listsarrays-and-tuples-in-python/
