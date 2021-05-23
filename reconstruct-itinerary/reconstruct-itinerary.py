97306b66f9eff75261ef99f217537ea9406be078
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-05-23 12:45:41 -0500

    Time: 72 ms (36.79%), Space: 13.9 MB (66.81%) - LeetHub

--- reconstruct-itinerary/reconstruct-itinerary.py
+++ reconstruct-itinerary/reconstruct-itinerary.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def findItinerary(self, tickets):
         self.adjList = collections.defaultdict(list)
-        self.result = []    
-        tickets.sort(key = lambda x:x[1])
         
+        tickets.sort(key = lambda x:x[1]) # since we need to maintain lexigraphical order we need it
+        print(tickets)
         for start, end in tickets:
             if start not in self.adjList:
                 self.adjList[start] = [end]
             else:
                 self.adjList[start].append(end)
+                
+        self.result = []
         self.dfs("JFK")
         return self.result[::-1]
     
