e3111e1dd7428840c0b208cbc2c5f94578c2ec3a
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-06-08 11:18:00 -0500

    Time: 548 ms (74.92%), Space: 13.6 MB (98.19%) - LeetHub

--- shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,28 +1,27 @@
-class Solution:
-    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        if grid[0][0] or grid[-1][-1]:
+class Solution(object):
+    def shortestPathBinaryMatrix(self, grid):
+        # base case
+        if grid[0][0] == 1 or grid[-1][-1] == 1:
             return -1
         
         m = len(grid)
         n = len(grid[0])
         queue = deque()
         queue.append((0,0,1))
-    
-        directions = [(0,1), (1,0), (-1,0), (0,-1), (1,1), (-1,-1), (1,-1), (-1,1)]
+        directions = [(-1,0),(0,-1),(1,0), (0,1), (1,1), (-1,-1), (-1,1), (1,-1)]
         
         while queue:
-            i, j , steps = queue.popleft()
-            if i == m - 1 and j == n - 1:
+            i, j, steps = queue.popleft()
+            
+            if i == m-1 and j == n - 1:
                 return steps
+            
             for dirs in directions:
                 r = dirs[0] + i
                 c = dirs[1] + j
-                if 0 <= r < m and 0<= c < n and not grid[r][c]: 
-                    queue.append((r, c, steps + 1))
+                if 0 <= r < m and 0<= c < n and grid[r][c] == 0:
+                    queue.append((r,c, steps + 1))
                     #print(queue)
                     grid[r][c] = "X"
-        return -1
-                
                     
-        
-        
+        return -1
