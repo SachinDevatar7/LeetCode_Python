0d7554c2b4fc5b1e2b509248b543c404a0537414
Sachin Devatar <sachindevatarhs777@gmail.com>
2021-09-05 22:45:33 -0500

    Time: 3552 ms (26.60%), Space: 14.3 MB (48.31%) - LeetHub

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -1,11 +1,22 @@
 class Solution(object):
     def twoSum(self, nums, target): 
-        dic = {}
-        
+        if len(nums) <= 0:
+            return False
         for i in range(len(nums)):
-            if nums[i] not in dic:
-                dic[target - nums[i]] = i
-            else:
-                return dic[nums[i]], i
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
+                
+        
+        
+        
+        
+#         dic = {}
+        
+#         for i in range(len(nums)):
+#             if nums[i] not in dic:
+#                 dic[target - nums[i]] = i
+#             else:
+#                 return dic[nums[i]], i
             
                 
